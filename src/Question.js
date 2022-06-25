5fdc74fbf6bb2f4c6f726131cb50b4f3d7e3a421
mehdi <hymehdi84@gmail.com>
2022-06-25 11:27:38 -0700

    end

--- src/Question.js
+++ src/Question.js
@@ -1,7 +1,23 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+const Question = ({ title, info }) => {
+  const [show, setShow] = useState(false);
+
+  return (
+    <div className='question'>
+      <header>
+        <h4>{title}</h4>
+        <button className='btn' onClick={() => setShow(!show)}>
+          {!show ? <AiOutlinePlus /> : <AiOutlineMinus />}
+        </button>
+      </header>
+      {show && (
+        <>
+          <p>{info}</p>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default Question;
